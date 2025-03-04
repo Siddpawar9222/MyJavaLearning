2ef61910a5002317285dcbb48906d6f0b6cb00a0
siddhesh <siddpawar583@gmail.com>
2025-03-04 23:31:57 +0530

    Intermediate operations stream

--- src/main/java/M_Java8/D_Stream/New/Employee.java
+++ src/main/java/M_Java8/D_Stream/New/Employee.java
@@ -1,8 +1,11 @@
 package M_Java8.D_Stream.New;
 
+import java.util.List;
+
 public class Employee {
     String name;
     Integer salary;
+    List<String> skills ;
 
     public Employee() {
 
@@ -11,6 +14,11 @@ public class Employee {
     public Employee(String name, Integer salary) {
         this.name = name;
         this.salary = salary;
+    }
+
+    public Employee(String name, List<String> skills){
+         this.name=name;
+         this.skills = skills;
     }
 
     public String getName() {
@@ -29,11 +37,20 @@ public class Employee {
         this.salary = salary;
     }
 
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
                 "name='" + name + '\'' +
                 ", salary=" + salary +
+                ", skills=" + skills +
                 '}';
     }
 }
---
+++ src/main/java/M_Java8/D_Stream/New/IntermediateOperations.java
@@ -0,0 +1,117 @@
+package M_Java8.D_Stream.New;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class IntermediateOperations {
+    public static void main(String[] args) {
+
+        List<Employee> employees = List.of(
+                new Employee("Siddhesh", 45222),
+                new Employee("Santosh", 50222),
+                new Employee("manisha", 41222),
+                new Employee("ram", 30422),
+                new Employee("karan", 6322),
+                new Employee("sweta", 786112)
+        );
+
+        List<Employee> employeesWithSkills = List.of(
+                new Employee("Siddhesh", List.of("java", "react")),
+                new Employee("Santosh", List.of("python", "angular")),
+                new Employee("manisha", List.of("testing")),
+                new Employee("ram", List.of("data science"))
+        );
+
+        /*
+         1. Filtering (filter(Predicate))
+         Used for : Removing unwanted elements from the stream.
+         */
+        List<Employee> filteredList = employees.stream()
+                .filter(employee -> employee.getSalary() > 40000)
+                .toList();
+
+        System.out.println(filteredList);
+
+        /*
+        2. Mapping (map(Function))
+        Used for: Transforming elements from one type to another.
+        */
+
+        List<String> employeesName = employees.stream()
+                .map(Employee::getName)
+                .toList();
+        System.out.println(employeesName);
+
+        /*
+        3. FlatMapping (flatMap(Function))
+        Used for: Flattening nested collections (List of Lists).
+        */
+        List<String> employeesSkills = employeesWithSkills.stream()
+                .flatMap(employee -> employee.getSkills().stream())
+                .toList();
+
+        System.out.println(employeesSkills);
+
+        /*
+        4. Sorting (sorted() and sorted(Comparator))
+        Used for : Sorting
+        */
+        List<Employee> sortedEmployeeList = employees.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .toList();
+
+        System.out.println(sortedEmployeeList);
+
+       /*
+       5. Distinct elements (distinct())
+       Used for: Removing duplicate elements.
+       */
+        List<String> categories = List.of("Electronics", "Clothing", "Electronics", "Books");
+
+        List<String> uniqueCategories = categories.stream()
+                .distinct()
+                .toList();
+
+        System.out.println(uniqueCategories);
+
+
+        /*
+         6. Limiting and Skipping (limit(long) & skip(long))
+             Used for:
+                     limit(n): Get only the first n elements.
+                     skip(n): Skip the first n elements and get the rest.
+        * */
+        List<Employee> top2 = employees.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed()) // Sort descending
+                .limit(2)
+                .toList();
+
+        System.out.println(top2);
+
+        List<Employee> remainingAllExceptTop2 = employees.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed()) // Sort descending
+                .skip(2)
+                .toList();
+
+        System.out.println(remainingAllExceptTop2);
+
+
+
+        /*
+        7. Peeking (peek(Consumer))
+           Used for: Debugging or logging data while processing.
+
+        * */
+        List<Employee> filteredListWithPeek = employees.stream()
+                .peek(employee -> System.out.println("Employee " + employee))
+                .filter(employee -> employee.getSalary() > 40000)
+                .toList();
+
+        System.out.println(filteredListWithPeek);
+
+
+    }
+}
+/*
+Intermediate operations are lazy operations that transform a stream into another stream. They don’t execute immediately—they only run when a terminal operation (like collect(), forEach(), count()) is applied.
+* */
